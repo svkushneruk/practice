f5f0b86c5c15a7c528031af301df9c676ad45d5f
svkushneruk <svkushneruk@ukr.net>
2019-09-03 16:21:00 +0300

    Первая практика

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let calculationStart = document.getElementById('start'),
+   budget = document.getElementsByClassName('budget-value'),
+   daybudget = document.getElementsByClassName('daybudget-value'),
+   level = document.getElementsByClassName('level-value'),
+   expenses = document.getElementsByClassName('expenses-value'),
+   optionalexpenses = document.getElementsByClassName('optionalexpenses-value'),
+   income = document.getElementsByClassName('income-value'),
+   monthsavings = document.getElementsByClassName('monthsavings-value'),
+   yearsavings = document.getElementsByClassName('yearsavings-value'),
+   expensesItem = document.getElementsByClassName('expenses-item'),
+   expensesItemBtn = document.getElementsByTagName('button')['0'],
+   optionalexpensesBtn = document.getElementsByTagName('button')['1'],
+   countBudgetBtn = document.getElementsByTagName('button')['2'],
+   optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+   chooseIncome = document.querySelector('.choose-income'),
+   savings = document.querySelector('#savings'),
+   sum = document.querySelector('#sum'),
+   percent = document.querySelector('#percent'),
+   year = document.querySelector('.year-value'),
+   month = document.querySelector('.month-value'),
+   day = document.querySelector('.day-value');
+
+
